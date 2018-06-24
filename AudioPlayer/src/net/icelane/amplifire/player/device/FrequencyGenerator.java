b845e10b3ed47af84949a10e0a7c49a23700c3c9
MrX13415 <mr.x.13415@gmail.com>
2018-06-24 14:58:46 +0200

    Freq gen no works as intended

--- AudioPlayer/src/net/icelane/amplifire/player/device/FrequencyGenerator.java
+++ AudioPlayer/src/net/icelane/amplifire/player/device/FrequencyGenerator.java
@@ -1,4 +1,4 @@
-package net.icelane.lolplayer.player.device;
+package net.icelane.amplifire.player.device;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
@@ -17,15 +18,16 @@ import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import net.icelane.lolplayer.Application;
-import net.icelane.lolplayer.gui.ui.UIFrame;
-import net.icelane.lolplayer.player.analyzer.Analyzer;
+import net.icelane.amplifire.Application;
+import net.icelane.amplifire.analyzer.Analyzer;
+import net.icelane.amplifire.ui.ui.UIFrame;
 
 public class FrequencyGenerator extends AudioDeviceLayer {
 
 	private static FrequencyGenerator currentFrequencyDeviceLayer = null;
 	private static SettingsUI settingsUI = null;
 	private int frequency = 432;
+	private float volume = 0.2f; 
 	
 	private Thread generatorThread;
 	
@@ -61,6 +63,24 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 
 	}
 	
+	@Override
+	public void setVolume(float vol) {
+		float vmax = 1f;
+		float vmin = 0f;
+		
+		//linear:
+		float vnew = (vmin + (vmax - vmin) / 1f * vol);
+			
+		//log:
+//		float vdelta = vmin - vmax;
+//		float vnew = (float) (Math.log(vol/100f) * (vdelta / Math.log(0.01f/100f)) + vmax);
+//		if (vnew > vmax) vnew = vmax;
+//		if (vnew < vmin) vnew = vmin;
+		System.out.println(vnew);
+		volume = vnew;
+	}
+	
+	
 	/**
 	 * 
 	 * 
@@ -75,7 +95,7 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 				
 				while(settingsUI != null)
 				{
-					byte[] b = monoToStereo(generateSineWave(frequency));
+					byte[] b = monoToStereo(generateSineWave(frequency, 1000, volume));
 					try {
 						write(this, b, 0, b.length);
 					} catch (LineUnavailableException e1) {						
@@ -124,10 +144,12 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 		private static final long serialVersionUID = -3673906249509467472L;
 		
 		private JSlider freqSilder;
+		private JSlider volSilder;
 		private JTextField freqTextField;
 		
 		private FrequencyGenerator frequencyGen;
 		private int lastFreqency = 432; //Hz
+		private int lastVolume = 20; 
 		
 		public SettingsUI(JFrame parent, FrequencyGenerator frequencyGen) {
 //			super(parent);
@@ -137,21 +159,35 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 			JLabel freqLabel = new JLabel("Tone frequency: ");
 			freqLabel.setForeground(Application.getColors().color_forground1);
 			
+			JLabel volLabel = new JLabel("Tone Volume: ");
+			volLabel.setForeground(Application.getColors().color_forground1);
+			
 			freqSilder = new JSlider(1, 1000, lastFreqency);
 			freqSilder.addChangeListener(this);
 			
-			freqTextField = new JTextField(freqSilder.getValue());
+			freqTextField = new JTextField();
+			freqTextField.setSize(5, 5);
 			freqTextField.addActionListener(this);
+			freqTextField.setText(String.valueOf(lastFreqency) + " Hz");
+			
+			volSilder = new JSlider(0, 100, lastVolume);
+			volSilder.addChangeListener(this);
 			
 			JPanel freqPanel = new JPanel(new BorderLayout());
 			freqPanel.setOpaque(false);
+			freqPanel.add(freqLabel, BorderLayout.NORTH);
 			freqPanel.add(freqSilder, BorderLayout.CENTER);
-			freqPanel.add(freqLabel, BorderLayout.EAST);
+			freqPanel.add(freqTextField, BorderLayout.SOUTH);
+			
+			JPanel volPanel = new JPanel(new BorderLayout());
+			volPanel.setOpaque(false);
+			volPanel.add(volLabel, BorderLayout.NORTH);
+			volPanel.add(volSilder, BorderLayout.CENTER);
 			
 			JPanel mp = new JPanel(new BorderLayout());
 			mp.setOpaque(false);
-			mp.add(freqLabel, BorderLayout.NORTH);
 			mp.add(freqPanel, BorderLayout.CENTER);
+			mp.add(volPanel, BorderLayout.SOUTH);
 			mp.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 			this.getTitleFrame().getResizeButton().setVisible(false);
@@ -176,7 +212,12 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 			if (e.getSource().equals(freqSilder)){
 				lastFreqency = freqSilder.getValue();
 				setFrequency(lastFreqency);
-				freqTextField.setText(String.valueOf(lastFreqency));
+				freqTextField.setText(String.valueOf(lastFreqency) + " Hz");
+			}
+			if (e.getSource().equals(volSilder)){
+				lastVolume = volSilder.getValue();
+				setVolume((float)lastVolume / 100f);
+				volSilder.setValue(lastVolume);
 			}
 		}
 
@@ -184,12 +225,23 @@ public class FrequencyGenerator extends AudioDeviceLayer {
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource().equals(freqTextField)){
 				try {
-					lastFreqency = Integer.valueOf(freqTextField.getText());
+					lastFreqency = Integer.valueOf(freqTextField.getText().replace("Hz", "").trim());
 				} catch (Exception e2) {
+					freqTextField.setText(String.valueOf(lastFreqency));
 				}
+				
+				if (lastFreqency < 0) {
+					lastFreqency = 0;
+					freqTextField.setText(String.valueOf(lastFreqency));
+				}
+				
+				if (lastFreqency < freqSilder.getMaximum())
+					freqSilder.setValue(lastFreqency);
+				
+				if (!freqTextField.getText().contains("Hz"))
+					freqTextField.setText(freqTextField.getText() + " Hz");
+				
 				setFrequency(lastFreqency);
-				freqSilder.setValue(lastFreqency);
-				freqTextField.setText(String.valueOf(freqSilder.getValue()));
 			}
 		}
 		
